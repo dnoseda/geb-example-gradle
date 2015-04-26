5ad70b19cba7417025d9f68d42ec6643e40a735d
dnoseda <damian.noseda@mercadolibre.com>
2015-04-26 18:41:59 -0300

    changing the report folder

--- src/test/groovy/GebishOrgTest.groovy
+++ src/test/groovy/GebishOrgTest.groovy
@@ -1,32 +1,46 @@
+import geb.Browser;
 import geb.junit4.GebReportingTest
+
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+
 import com.saucelabs.junit.Parallelized;
+
 import org.junit.runners.Parameterized.Parameters
 
 @RunWith(Parallelized.class)
 class GebishOrgTest extends GebReportingTest {
-	
-	
+
+	Browser createBrowser() {
+
+		println "creando browser"
+		def conf = new geb.Configuration()
+		File reportsDir = new File("build/reports/test/testReportFor-${Thread.currentThread().getId()}")
+		conf.driver = driver
+		conf.reportsDir = reportsDir
+		new geb.Browser(conf)
+	}
+
+
 
 	def toSearch = null
-	
+
 	@Parameters
 	static data(){
 		def dataList = []
-		["dulce","tortita"].each{ toQuery ->
+		["dulce", "tortita"].each{ toQuery ->
 			dataList << ([toQuery] as Object[])
 		}
-		
-		
+
+
 		return dataList
 	}
-	
+
 	public GebishOrgTest(String toQuery){
 		toSearch = toQuery
 	}
-	
+
 	@Test
 	void canGetToTheCurrentBookOfGeb() {
 		to GooglePage
@@ -34,7 +48,7 @@ class GebishOrgTest extends GebReportingTest {
 		println "buscando '${toSearch}'"
 		//hover over to expand the menu
 		query = "${toSearch}\n"
-		 
+
 		println "buscando '${toSearch}' Termino!"
 	}
 }
