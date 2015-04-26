d6a10866e60387c2d28af16b53f8c7abb040e284
dnoseda <damian.noseda@mercadolibre.com>
2015-04-26 19:18:15 -0300

    Work but with the whole geb

--- src/test/groovy/GebishOrgTest.groovy
+++ src/test/groovy/GebishOrgTest.groovy
@@ -8,19 +8,19 @@ import org.junit.runners.JUnit4
 import com.saucelabs.junit.Parallelized;
 
 import org.junit.runners.Parameterized.Parameters
+import org.codehaus.groovy.runtime.StackTraceUtils
+import org.apache.commons.lang.exception.ExceptionUtils
 
 @RunWith(Parallelized.class)
 class GebishOrgTest extends GebReportingTest {
 
-	Browser createBrowser() {
-
-		println "creando browser"
-		def conf = new geb.Configuration()
-		File reportsDir = new File("build/reports/test/testReportFor-${Thread.currentThread().getId()}")
-		conf.driver = driver
-		conf.reportsDir = reportsDir
-		new geb.Browser(conf)
+	static def logException(def msg){
+		println "desde donde llamo ${msg}"
+		println ExceptionUtils.getFullStackTrace(StackTraceUtils.deepSanitize(new Exception()))
+		println "desde donde llamo termina ${msg}"
 	}
+
+
 
 
 
@@ -45,10 +45,10 @@ class GebishOrgTest extends GebReportingTest {
 	void canGetToTheCurrentBookOfGeb() {
 		to GooglePage
 
-		println "buscando '${toSearch}'"
+		println "buscando ${threadId} '${toSearch}'"
 		//hover over to expand the menu
 		query = "${toSearch}\n"
 
-		println "buscando '${toSearch}' Termino!"
+		println "buscando ${threadId} '${toSearch}' Termino!"
 	}
 }
